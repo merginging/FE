492016acd7cd283fbec135c01cbd413666d64732
hyuke81 <rmsidthgusdl81@gmail.com>
2025-01-23 16:13:56 +0900

    feat: 소셜 로그인 추가 정보 입력 페이지 라우터 추가

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home/Home';
 import Price from './pages/Price/Price.jsx';
 import Login from './pages/Login/Login.jsx';
 import Signup from './pages/Signup/Signup.jsx';
+import Social from './pages/Signup/SocialSignup.jsx';
 
 const router = createBrowserRouter([
   {
@@ -25,6 +26,10 @@ const router = createBrowserRouter([
       {
         path: 'signup',
         element: <Signup />,
+      },
+      {
+        path: 'social',
+        element: <Social />,
       }
     ],
   },
