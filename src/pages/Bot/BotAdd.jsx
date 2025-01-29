39ae77ff1bbc1d097a6ed83a31f9aa2ceb2cc7aa
hyuke81 <rmsidthgusdl81@gmail.com>
2025-01-29 23:18:00 +0900

    feat: 봇 추가 페이지 구현

---
+++ src/pages/Bot/BotAdd.jsx
@@ -0,0 +1,35 @@
+/** @jsxImportSource @emotion/react */
+import { useNavigate } from 'react-router-dom';
+import * as S from './BotAdd.style';
+
+const BotAdd = () => {
+    const navigate = useNavigate();
+
+    return (
+        <div css={S.pageStyle}>
+        <div css={S.textStyle}>
+            <h1 css={S.titleStyle}>봇 관리하기</h1>
+            <p css={S.descriptionStyle}>
+                나만의 봇을 만들어서 관리해보세요 <br />
+                블라블라 당신의 워크플로우를 부스트 업 해줄거에요
+            </p>
+        </div>
+
+        <div css={S.containerStyle}>
+            <div css={S.botTextStyle}>
+                <p css={S.botCountStyle}>0/0 Bot</p>
+                <div css={S.addButtonStyle} onClick={() => navigate('/bot/ai')}>
+                <span css={S.addButtonTextStyle}>+ 새로운 봇 생성하기</span>
+                </div>
+            </div>
+            
+
+            <div css={S.dottedBoxStyle}>
+            <p css={S.emptyBotTextStyle}>나만의 코워킹 비서를 만들어보세요!</p>
+            </div>
+        </div>
+        </div>
+    );
+};
+
+export default BotAdd;
---
+++ src/pages/Bot/BotAdd.style.js
@@ -0,0 +1,121 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+
+export const pageStyle = css`
+    background: #fbfbfb;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 50px;
+    margin-left: 95px;
+`;
+
+export const textStyle = css`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: left;
+`
+
+export const titleStyle = css`
+    color: #000;
+    text-align: center;
+    font-family: 'Pretendard-SemiBold';
+    font-size: 36px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 22px;
+    margin-bottom: 19px;
+`;
+
+export const descriptionStyle = css`
+    color: #252525;
+    font-family: 'Pretendard-SemiBold';
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+    letter-spacing: -0.8px;
+    text-align: left;
+    margin-bottom: 30px;
+`;
+
+export const containerStyle = css`
+    width: 86%;
+    height: 60%;
+    flex-shrink: 0;
+    border-radius: 10px;
+    border: 1px solid #D6D6D6;
+    background: #FFF;
+    padding: 30px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+`;
+
+export const botTextStyle = css`
+    display: flex;
+    flex-direction: row;
+    gap: 10px;
+`;
+
+export const botCountStyle = css`
+    color: #000;
+    text-align: center;
+    font-family: 'Pretendard-reguler';
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 22px;
+    margin-top: 25px;
+`;
+
+export const addButtonStyle = css`
+    width: 180px;
+    height: 52px;
+    flex-shrink: 0;
+    border-radius: 5px;
+    background: #F1502F;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    transition: background 0.2s ease-in-out;
+    
+    &:hover {
+        background: #d9442a;
+    }
+`;
+
+export const addButtonTextStyle = css`
+    color: #FFF;
+    text-align: center;
+    font-family: 'Pretendard-SemiBold';
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 22px;
+`;
+
+export const dottedBoxStyle = css`
+    width: 100%;
+    height: 400px;
+    flex-shrink: 0;
+    border-radius: 10px;
+    border: 1px dashed #5F6368;
+    background: #FFF;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top: 20px;
+`;
+
+export const emptyBotTextStyle = css`
+    color: #252525;
+    text-align: center;
+    font-family: "Pretendard Variable";
+    font-size: 20px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 22px;
+    letter-spacing: -1px;
+`;
