7041d87611c2764cbac1100fd044bce70c88a704
hyuke81 <rmsidthgusdl81@gmail.com>
2025-02-07 02:51:00 +0900

    refactor: botset

--- src/pages/Bot/BotAi.jsx
+++ src/pages/Bot/BotAi.jsx
@@ -1,17 +1,21 @@
 /** @jsxImportSource @emotion/react */
 import { useState } from 'react';
 import BotStep1 from '../../components/BotAi/BotStep1';
-import BotStep2 from '../../components/BotAi/BotStep2';
+//import BotStep2 from '../../components/BotAi/BotStep2';
 //import BotStep3 from '../../components/BotAi/BotStep3';
 
+/*
+
+{step === 2 && <BotStep2 onNext={() => setStep(3)} onPrev={() => setStep(1)} />}
+            {step === 3 && <BotStep3 onPrev={() => setStep(2)} />}
+
+*/
 const BotAi = () => {
     const [step, setStep] = useState(1);
 
     return (
         <div>
             {step === 1 && <BotStep1 onNext={() => setStep(2)} />}
-            {step === 2 && <BotStep2 onNext={() => setStep(3)} onPrev={() => setStep(1)} />}
-            {step === 3 && <BotStep3 onPrev={() => setStep(2)} />}
         </div>
     );
 };
